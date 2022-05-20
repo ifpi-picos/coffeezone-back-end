a1a1edcf0f0567e5f81d4d4f1c2ae4bf4f1fee2b
Alessandro Viana <silvaalessandro352@gmail.com>
2022-05-19 22:59:24 -0300

    v1.6.0 - Criada rota relatorio

--- system/modules/db/repository/relatory.repository.js
+++ system/modules/db/repository/relatory.repository.js
@@ -1,11 +1,31 @@
 const { relatorys } = require('../models')
 
 module.exports = class RelatorysRepository {
-    constructor(){
-        return{
-            name: "relatorys",
-            functions: {
-            }
-        }
+  constructor () {
+    return {
+      name: 'relatory',
+      functions: {
+        create: this.create,
+        getByUserId: this.getByUserId,
+        updateActionsByUserId: this.updateActionsByUserId,
+        deleteByUserId: this.deleteByUserId
+      }
     }
+  }
+
+  async create (newRelatory) {
+    return await relatorys.create(newRelatory)
+  }
+
+  async getByUserId (userid) {
+    return await relatorys.findOne({ where: { userid } })
+  }
+
+  async updateActionsByUserId (userid, newAction) {
+    return await relatorys.update({ actions: newAction }, { where: { userid } })
+  }
+
+  async deleteByUserId (userid) {
+    return await relatorys.destroy({ where: { userid } })
+  }
 }
--- system/routes/relatory.route.js
+++ system/routes/relatory.route.js
@@ -1,11 +1,39 @@
 module.exports = class RelatoryRoute {
-    constructor(app) {
-        const { Router } = require('express');
-        const routes = Router()
+  constructor (app) {
+    const { Router } = require('express')
+    const routes = Router()
 
-        
+    routes.post('/', async (req, res) => {
+      try {
+          if(!req.body.cardid){
+            return res.status(400).json({error: 'ID do cartão faltando'})
+          }
+        const user = await app.db.user.getByCardId(req.body.cardid)
 
+        if (user) {
+          const relatory = await app.db.relatory.getByUserId(user.id)
+          const actions = app.services.relatory.createAction(
+            relatory ? relatory.actions : null
+          )
 
-        return { path: '/relatory/', router: routes }
-    }
+          if (relatory) {
+            await app.db.relatory.updateActionsByUserId(user.id, actions)
+          } else {
+            await app.db.relatory.create({
+              userid: user.id,
+              actions: actions
+            })
+          }
+          res.status(200).json({ sucess: "Ação adicionada" })
+        } else {
+          res.status(404).json({ error: 'Usuario não encontrado' })
+        }
+      } catch (err) {
+        app.log.error(err)
+        res.status(500).json({ error: err })
+      }
+    })
+
+    return { path: '/relatory/', router: routes }
+  }
 }
