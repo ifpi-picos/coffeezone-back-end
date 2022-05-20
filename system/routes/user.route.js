8520a0e86ba3ab1ba9c94afa47cf3a1ddf73786b
alanfilho184 <alan.filho@hotmail.com>
2022-05-20 14:09:59 -0300

    v1.6.2 - Alteração nas permissões do cordenador

--- system/routes/user.route.js
+++ system/routes/user.route.js
@@ -72,24 +72,7 @@ module.exports = class UserRoute {
 
         routes.patch('/', async (req, res) => {
             try {
-                let user
-                if (req.user.type == 'Coordinator') {
-                    if (req.body.email) {
-                        user = await app.db.user.getByEmail(req.body.email)
-                    }
-                    else if (req.body.id) {
-                        user = await app.db.user.getById(req.body.id)
-                    }
-                    else if (req.body.cardid) {
-                        user = await app.db.user.getByCardId(req.body.cardid)
-                    }
-                    else {
-                        user = await app.db.getById(req.user.id)
-                    }
-                }
-                else {
-                    user = await app.db.user.getById(req.user.id)
-                }
+                let user = await app.db.user.getById(req.user.id)
 
                 if (!user) {
                     res.status(404).json({ error: 'Usuário não encontrado' })
@@ -127,48 +110,12 @@ module.exports = class UserRoute {
 
         routes.delete('/', async (req, res) => {
             try {
-                if (req.user.type == 'Coordinator') {
-                    if (!req.body.id && !req.body.email && !req.body.cardid) {
-                        res.status(400).json({ error: 'Nenhum ID informado' })
-                    }
-                    else {
-                        if (req.body.id) {
-                            if (await app.db.user.getById(req.body.id)) {
-                                await app.db.user.deleteById(req.body.id)
-                                res.status(200).json({ success: 'Usuário deletado' })
-                            }
-                            else {
-                                res.status(404).json({ error: 'Usuário não encontrado' })
-                            }
-                        }
-                        else if (req.body.email) {
-                            if (await app.db.user.getByEmail(req.body.email)) {
-                                await app.db.user.deleteByEmail(req.body.email)
-                                res.status(200).json({ success: 'Usuário deletado' })
-                            }
-                            else {
-                                res.status(404).json({ error: 'Usuário não encontrado' })
-                            }
-                        }
-                        else if (req.body.cardid) {
-                            if (await app.db.user.getByCardId(req.body.cardid)) {
-                                await app.db.user.deleteByCardId(req.body.cardid)
-                                res.status(200).json({ success: 'Usuário deletado' })
-                            }
-                            else {
-                                res.status(404).json({ error: 'Usuário não encontrado' })
-                            }
-                        }
-                    }
+                if (await app.db.user.getById(req.user.id)) {
+                    await app.db.user.deleteById(req.user.id)
+                    res.status(200).json({ success: 'Usuário deletado' })
                 }
                 else {
-                    if (await app.db.user.getById(req.user.id)) {
-                        await app.db.user.deleteById(req.user.id)
-                        res.status(200).json({ success: 'Usuário deletado' })
-                    }
-                    else {
-                        res.status(404).json({ error: 'Usuário não encontrado' })
-                    }
+                    res.status(404).json({ error: 'Usuário não encontrado' })
                 }
             }
             catch (err) {
