4d4ab06a4a6296be79132f1f76d38497ebf23d96
alanfilho184 <alan.filho@hotmail.com>
2022-05-30 21:40:20 -0300

    v1.10.1 - Finalizada a rota de redefinir senha

--- system/routes/auth.route.js
+++ system/routes/auth.route.js
@@ -35,7 +35,7 @@ module.exports = class AuthRoute {
 
         routes.post("/recovery", async (req, res) => {
             try {
-                if (req.body.email) {
+                if (req.body.email && !req.body.token) {
                     let user = await app.db.user.getByEmail(req.body.email)
 
                     if (user) {
@@ -44,14 +44,35 @@ module.exports = class AuthRoute {
                         const mailInfo = await app.mailer.sendRecoveryEmail(user, token)
 
                         if (mailInfo.accepted.includes(user.email)) {
-                            res.status(200).json({ message: "Email enviado com sucesso" })
+                            res.status(200).json({ success: "Email enviado com sucesso" })
                         }
                         else {
-                            res.status(500).json({ error: "Email enviado mas não aceito pelo usuário" })
+                            res.status(400).json({ error: "Email enviado mas não aceito pelo usuário" })
                         }
                     }
                     else {
                         res.status(404).json({ error: "Usuário não encontrado" })
+                    }
+                }
+                else if (req.body.token && !req.body.email) {
+                    let token = app.services.auth.verifyToken(req.body.token)
+
+                    if (req.body.password) {
+                        const user = await app.db.user.getById(token.id)
+
+                        if (user) {
+                            const newPassword = app.services.user.hashPassword(req.body.password)
+
+                            await app.db.user.updateFieldById(user.id, "password", newPassword)
+
+                            res.status(200).json({ success: "Senha alterada com sucesso" })
+                        }
+                        else {
+                            res.status(404).json({ error: "Usuário não encontrado" })
+                        }
+                    }
+                    else {
+                        res.status(400).json({ error: "Nova senha não encontrada" })
                     }
                 }
                 else {
@@ -59,8 +80,16 @@ module.exports = class AuthRoute {
                 }
             }
             catch (err) {
-                app.log.error(err)
-                res.status(500).json({ error: "Erro ao tentar enviar email de recuperação" })
+                if (err.name = "TokenExpiredError") {
+                    res.status(401).json({ error: "Token expirado" })
+                }
+                else if (err.name = "JsonWebTokenError") {
+                    res.status(401).json({ error: "Token inválido" })
+                }
+                else {
+                    app.log.error(err)
+                    res.status(500).json({ error: "Erro ao tentar enviar email de recuperação" })
+                }
             }
         })
 
