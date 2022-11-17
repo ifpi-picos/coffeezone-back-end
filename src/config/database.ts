88c1a973efb88fc557be3bd90a30952d3d643ba2
Italo Paixão Gomes <italopaxg@gmail.com>
2022-11-17 12:32:59 -0300

    voltando a instância do prisma a versão anterior

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,20 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-// export const prisma = new PrismaClient();
-
-import { PrismaClient } from '@prisma/client'
-
-let db;
-
-//check if we are running in production mode
-if (process.env.NODE_ENV === 'production') {
-  db = new PrismaClient()
-} else {
-//check if there is already a connection to th  e database
-  if (!global.db) {
-    global.db = new PrismaClient()
-  }
-  db = global.db
-}
-
-export { db };
+export const prisma = new PrismaClient();
